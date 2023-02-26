cf39370fdeb910b4941a791f8addf4aebf0e1264
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-26 15:02:08 +0100

    test delete account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -65,3 +65,14 @@ func TestUpdateAccount(t *testing.T) {
 	require.Equal(t, account1.Currency, account2.Currency)
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.Empty(t, account2)
+}
