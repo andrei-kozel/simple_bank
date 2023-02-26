6219f91859bf355929e1f039bfbfb3117961d8e3
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-26 14:55:15 +0100

    generate random data

---
+++ util/random.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt returns a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max - min + 1)
+}
+
+// RandomString returns a random string of the given length
+func RandomString(length int) string {
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(result)
+}
+
+func RandomOwnerName() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(1, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "SEK", "NZD"}
+	return currencies[rand.Intn(len(currencies))]
+}
